802269effed72606ee25cf08c45a8a39a46e28c8
FellSalgado <salgadocanga@gmail.com>
2022-11-03 17:08:56 -0500

    se mejoro el diseño responsivo

--- js/script.js
+++ js/script.js
@@ -27,3 +27,14 @@ navWrapper.addEventListener("click", e => {
     toggleButton.classList.remove("close");
   }
 });
+
+window.addEventListener('scroll', function()  {
+    let element = document.getElementById('scroll-content');
+    let screenSize = window.innerHeight;
+  
+    if(element.getBoundingClientRect().top < screenSize) {
+      element.classList.add('visible');
+    } else {
+      element.classList.remove('visible');
+    }
+});
