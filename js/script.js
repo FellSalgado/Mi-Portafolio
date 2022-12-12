d509010bdeb2da874c1840eb2d2ffce3ec5bf249
FellSalgado <salgadocanga@gmail.com>
2022-12-12 14:46:10 -0500

    se modifico el menu hamburguesa

--- js/script.js
+++ js/script.js
@@ -54,3 +54,26 @@ window.addEventListener('scroll', scrollParallax);
       element.classList.remove('visible');
     }
 }); */
+
+
+/* ----------- &copy --------- */
+const fecha = Date.now();
+const hoy = new Date(fecha);
+const year = hoy.getFullYear();
+
+let footer = document.getElementById('year');
+footer.innerHTML = year;
+
+function mostrar() {
+    let table = document.getElementById('table').value;
+    let row = document.getElementById('row').value;
+    let respuesta = document.getElementById('respuesta');
+
+    if (table >= 0 && table <= 20) {
+        for (let i = 1; i <= row; i++) {
+            const resultado = document.createElement('p');
+            resultado.innerHTML = table + ' X ' + i + ' = ' + table * i;
+            respuesta.appendChild(resultado);
+        }
+    }
+}
